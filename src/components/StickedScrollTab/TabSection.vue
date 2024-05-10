db64d6e8a74446d16912f7024cff0270afc01281
Бакдаулет Болат <bolat@Bakdaulets-MacBook-Air.local>
2024-05-10 22:41:04 +0500

    fixing errors 10 march

--- src/components/StickedScrollTab/TabSection.vue
+++ src/components/StickedScrollTab/TabSection.vue
@@ -6,7 +6,7 @@ defineProps<{
 </script>
 
 <template>
-  <div class="unselectable py-[14px] text-sm text-nowrap text-[#66666E] px-[19.2px] rounded-[16px] cursor-pointer" :class="{
+  <div class="unselectable white-space-pre  py-[14px] text-sm text-nowrap text-[#66666E] px-[19.2px] rounded-[16px] cursor-pointer" :class="{
             'bg-[#202020] !text-white': isActive
         }">
     {{ name }}
@@ -14,5 +14,7 @@ defineProps<{
 </template>
 
 <style scoped>
-
+.white-space-pre {
+  white-space: pre;
+}
 </style>
