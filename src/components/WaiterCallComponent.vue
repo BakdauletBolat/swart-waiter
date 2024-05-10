860894389d4437cc71395eefaaf7d5e0dcdc2134
Бакдаулет Болат <bolat@Bakdaulets-MacBook-Air.local>
2024-05-10 22:18:10 +0500

    fixing errors 10 march

--- src/components/WaiterCallComponent.vue
+++ src/components/WaiterCallComponent.vue
@@ -6,7 +6,6 @@ import {closeWaiterCall, waiter} from "./SidebarModal/index.ts";
 </script>
 <template>
   <div class="flex justify-center items-center flex-col p-4">
-    {{waiter}}
     <Image class="h-[122px] w-[122px]" :url="waiter?.attributes != undefined ? waiter?.attributes?.logo : undefined"></Image>
     <h2 class="text-2xl mt-4">{{waiter?.attributes != null ? waiter?.attributes?.first_name : 'Загрузка'}} в пути</h2>
     <p class="text-sm text-[#9999A1] text-center max-w-[350px] mt-4">Официант увидел что вы его позвали, скоро он подойдет к вам</p>
