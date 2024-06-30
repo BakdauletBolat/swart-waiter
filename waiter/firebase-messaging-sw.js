90d3a969c361e929afb75a8f0b28f7f7eb6db3f6
Bakdaulet <bakosh21345@gmail.com>
2024-06-30 17:17:04 +0500

    adding logo

--- waiter/firebase-messaging-sw.js
+++ waiter/firebase-messaging-sw.js
@@ -17,7 +17,7 @@ messaging.onBackgroundMessage(function(payload) {
     const notificationTitle = payload.notification.title;
     const notificationOptions = {
         body: payload.notification.body,
-        icon: '/firebase-logo.png'
+        icon: '/logo.png'
     };
 
     self.registration.showNotification(notificationTitle,
